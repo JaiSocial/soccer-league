6c5b61d6114b12c85d2ed084f13a7252544f7e3f
Jai Social <sundaram.medimmune@gmail.com>
2017-08-22 16:24:49 -0400

    Adds use of Bootstrap Implements support for Season details

--- client/soccer/seasons/SeasonDetails.js
+++ client/soccer/seasons/SeasonDetails.js
@@ -2,18 +2,132 @@ Template.SeasonDetails.onCreated(function (){
 
 	var self = this;
 	self.autorun(function(){
-		// self.subscribe('seasons');
+		self.subscribe('seasons');
+		self.subscribe('games');
+		self.subscribe('teams');
+		self.subscribe('locations');
 	})
 });
 
 Template.SeasonDetails.helpers({
+	
 	season: () => {
-		var id = FlowRouter.getParam('id');
-		console.log(id);
-		return Seasons.findOne({season_id: id});
-		// var season_doc = Seasons.findOne({'season_id' : {$eq : id}});
-		// console.log(season_doc);
-		// console.log(Seasons.findOne({'season_id': id}));
-		// return season_doc;
+		
+		const id = FlowRouter.getParam('id');
+
+		const season_id = parseInt(id);
+
+		let season = Seasons.findOne({"season_id": season_id});
+
+		season.games = _getSeasonGames(season_id);
+
+		return season;
 	}
 });
+
+
+function _getLocationIdToNameLookup(){
+
+	const location_documents = Locations.find({});
+
+	let location_lookup = {};
+
+	/*------------------------------------------------ 
+		Build the location_id to location name lookup 
+	------------------------------------------------*/
+	location_documents.forEach(function(location){
+		
+		const id  = location.location_id;
+		
+		const name = location.name;
+		
+		location_lookup[id] = name;
+	});
+
+	console.log("loaded location lookup");
+
+	return location_lookup;	
+}
+
+function _getTeamIdToNameLookup(){
+
+	/*----------------------------------------------- 
+		Retrieve the team documents 
+	-----------------------------------------------*/
+	const team_documents = Teams.find({});
+
+	let team_lookup = {};
+
+	/*------------------------------------------------ 
+		Build the team_id to team name lookup 
+	------------------------------------------------*/
+	team_documents.forEach(function(team){
+		
+		const team_id  = team.team_id;
+		
+		const team_name = team.name;
+		
+		team_lookup[team_id] = team_name;
+	});
+
+
+	console.log("loaded team lookup");
+
+	return team_lookup;
+}
+
+
+function _getSeasonGames(season_id){
+
+	const location_lookup = _getLocationIdToNameLookup();
+
+	const team_lookup = _getTeamIdToNameLookup();
+
+	const game_documents = Games.find({});
+
+	let game_list = [];
+	
+	let game_ctr = 0;
+
+	game_documents.forEach(function(game){
+
+		if (team_lookup[game.home_team_id] === undefined){
+			throw new Error("home team id '" + game.home_team_id + "' does not exist in the team lookup");
+		}
+
+		const home_team_name = team_lookup[game.home_team_id];
+	
+
+		if (team_lookup[game.away_team_id] === undefined){
+			throw new Error("away team id '" + game.away_team_id + "' does not exist in the team lookup");
+		}
+
+		const away_team_name = team_lookup[game.away_team_id];
+
+		if (location_lookup[game.location_id] === undefined){
+			throw new Error("location id '" + game.location_id + "' does not exist in the location lookup");
+		}
+
+		const location_name = location_lookup[game.location_id];
+
+
+		let new_game = {
+			"home_team_name" : home_team_name,
+			"away_team_name" : away_team_name,
+			"home_team_id" : game.home_team_id,
+			"away_team_id" : game.away_team_id,
+			"date" : game.date,
+			"location_id" : game.location_id,
+			"location_name" : location_name
+		};
+
+		game_list.push(new_game);
+
+		game_ctr++;
+
+	});
+
+	console.log("loaded game list (" + game_ctr + " records)");
+
+	return game_list;
+}
